c2d0be68660646cd8754df5f095a595d2c1d0209
jsbin0526 <jsbin0526@gmail.com>
2022-05-19 14:46:31 +0900

    10768

---
+++ 10768.py
@@ -0,0 +1,12 @@
+m, d = map(int, [input(), input()])
+if m > 2:
+    print("After")
+elif m < 2:
+    print("Before")
+else:
+    if d > 18:
+        print("After")
+    elif d < 18:
+        print("Before")
+    else:
+        print("Special")
