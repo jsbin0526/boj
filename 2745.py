88aa7fa0c54ce64ef5f23f26352e5aba87d8f4c8
jsbin0526 <jsbin0526@gmail.com>
2023-03-31 11:08:32 +0900

    2745

--- 2745.py
+++ 2745.py
@@ -1,2 +1,10 @@
 n, b = input().split()
-print(int(n, b))
+dec = 0
+b = int(b)
+for i in range(len(n)):
+    if n[i].isalpha():
+        x = ord(n[i])-55
+    else:
+        x = int(n[i])
+    dec += x*b**(len(n)-i-1)
+print(dec)
