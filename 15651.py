9b132ae061d003305763c7e37247c7ca22aba5d0
jsbin0526 <jsbin0526@gmail.com>
2024-11-19 17:31:41 +0900

    15651

--- 15651.py
+++ 15651.py
@@ -1,13 +1,13 @@
 s = []
 n, m = map(int, input().split())
 
-def dfs():
+def dfs(start):
     if len(s) == m:
         print(*s)
         return
-    for i in range(1, n+1):
+    for i in range(start, n+1):
         s.append(i)
-        dfs()
+        dfs(i)
         s.pop()
     
-dfs()
+dfs(1)
